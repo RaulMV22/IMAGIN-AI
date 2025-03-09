8a40658a1ee59d8850211f5bb18336cb038ebd62
RaulMV22 <150440171+RaulMV22@users.noreply.github.com>
2025-03-09 23:02:07 +0100

    subida de test

---
+++ jest.config.js
@@ -0,0 +1,13 @@
+const nextJest = require("next/jest");
+
+const createJestConfig = nextJest({ dir: "./" });
+
+const customJestConfig = {
+  setupFilesAfterEnv: ["<rootDir>/jest.setup.js"],
+  testEnvironment: "jest-environment-jsdom",
+  transform: {
+    "^.+\\.(js|jsx|ts|tsx)$": "babel-jest"
+  }
+};
+
+module.exports = createJestConfig(customJestConfig);
